95e971afa9d00613006a5672bda22b6185656b01
vzhz <vehanus@gmail.com>
2016-11-04 14:50:30 -0400

    initial commit of make my card, function to ask user preferance

---
+++ make_my_card.py
@@ -0,0 +1,12 @@
+### function that takes associative list (sorted so paragraphs are in order, list of key-value pairs
+### for paragraphs, each person represented by set of interests)
+
+SUBJECT_ENTRY =
+
+def ask_user_interest():
+    user_interest = raw_input("""Where do I know you from? Space Camp,
+                                                           Colgate,
+                                                           JPL,
+                                                           MIT, and/or
+                                                           Recurse Center?""")
+
