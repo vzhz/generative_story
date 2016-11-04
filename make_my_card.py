042fcab5d31baeb43550adc9b6a65d3a67d1a405
vzhz <vehanus@gmail.com>
2016-11-04 15:17:56 -0400

    add user name and interest to ask user function

--- make_my_card.py
+++ make_my_card.py
@@ -3,10 +3,13 @@
 
 SUBJECT_ENTRY =
 
-def ask_user_interest():
-    user_interest = raw_input("""Where do I know you from? Space Camp,
+def ask_user_identity():
+    user_name = raw_input("""What is your name?""") # they can enter anything
+    user_origin = raw_input("""Where do I know you from?   Space Camp,
                                                            Colgate,
                                                            JPL,
                                                            MIT, and/or
-                                                           Recurse Center?""")
+                                                           Recurse Center?""") #they can enter anything although I made suggestions
+    user_interest = raw_input("""When we get together, what do we like to talk about? (list all, spell precisely)
+                                 Programming, Home Projects, Geology Jokes, Shit Trump Says, Stuff We Like On The Internet, Relationship Things""")
 
